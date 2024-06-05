9bb27fbee846809e77ffe909190e7fca9e4ba4a2
felabel <felicityabel2016@gmail.com>
2024-06-05 21:37:18 +0100

    test

--- frontend/src/context/RecipesContext.jsx
+++ frontend/src/context/RecipesContext.jsx
@@ -1,36 +1,3 @@
-// src/RecipesContext.js
-// import React, { createContext, useState, useEffect } from "react";
-// import { fetchAllRecipes } from "../API/api";
-
-// export const RecipesContext = createContext();
-
-// export const RecipesProvider = ({ children }) => {
-//   const [recipes, setRecipes] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const getRecipes = async () => {
-//       try {
-//         const allRecipes = await fetchAllRecipes();
-//         setRecipes(allRecipes);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Failed to fetch recipes:", error);
-//         setLoading(false);
-//       }
-//     };
-
-//     getRecipes();
-//   }, []);
-//   // console.log("meals", recipes);
-
-//   return (
-//     <RecipesContext.Provider value={{ recipes, loading }}>
-//       {children}
-//     </RecipesContext.Provider>
-//   );
-// };
-
 import React, { createContext, useState, useEffect } from "react";
 import { fetchAllRecipes } from "../API/api";
 
